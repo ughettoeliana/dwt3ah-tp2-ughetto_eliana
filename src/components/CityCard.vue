2398e0f9b41097debada7d1f6740a5cb63cadbff
Eliana Ughetto <elianaughetto10@gmail.com>
2022-12-01 02:00:49 -0300

    added a remove function to the component

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="components-demo">
-    <div class="cityHistory-div" v-for="(item, index) in cityHistory.slice().reverse()" v-bind:key="index"
->
+    <div id="cityDiv" class="cityHistory-div" v-for="(item, index) in cityHistory.slice().reverse()" v-bind:key="index">
       <div class="cityHistory-box">
         <h2 class="cityHistory-title">{{ item.name }}</h2>
         <h3 class="cityHistory-temp">{{ item.temp }}°c</h3>
         <p class="cityHistory-weather">{{ item.weather }}</p>
       </div>
+      <div class="text-center">
+      </div>
+      <v-btn class="mx-2" dark small v-on:click="remove(index)">
+        <i class='bx bx-trash'></i>
+        <span>Delete </span>
+      </v-btn>
     </div>
   </div>
 </template>
@@ -19,6 +24,20 @@ export default {
     'temp',
     'weather'
   ],
+  data: () => {
+    return {
+      hasError: false,
+    }
+  },
+  methods: {
+     remove() {
+      let removeCity = document.getElementById("cityDiv");
+      removeCity.remove();
+      this.hasError = true;
+      return;
+    },
+
+  },
 }
 
 </script>
